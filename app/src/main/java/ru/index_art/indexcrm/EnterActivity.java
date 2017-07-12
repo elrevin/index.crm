5a82e846150539fa5bf1ed924a2a039b1e1ad397
elrevin <elrevin@gmail.com>
2017-07-12 23:30:57 +0500

    first

---
+++ app/src/main/java/ru/index_art/indexcrm/EnterActivity.java
@@ -0,0 +1,13 @@
+package ru.index_art.indexcrm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter);
+    }
+}
