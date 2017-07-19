1c6c981cff01e0f0f549b0cc7125f13398e93811
elrevin <elrevin@gmail.com>
2017-07-20 00:19:57 +0500

    some

--- app/src/main/java/ru/index_art/indexcrm/activities/main/MainActivity.java
+++ app/src/main/java/ru/index_art/indexcrm/activities/main/MainActivity.java
@@ -2,14 +2,53 @@ package ru.index_art.indexcrm.activities.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import ru.index_art.indexcrm.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putString("ent",((EditText)findViewById(R.id.edt)).getText().toString());
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        ((EditText)findViewById(R.id.edt)).setText(savedInstanceState.getString("edt"));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("index.art", "resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("index.art", "stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("index.art", "destroy");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("index.art", "start");
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("index.art", "create");
     }
 }
